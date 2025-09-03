a8c2d1312b78b53a0ae36801f66edab73b3ac63c
Guilherme <uchihaguilherme4@gmail.com>
2025-09-02 23:05:24 -0300

    adiciona service execution role

---
+++ iam.tf
@@ -0,0 +1,42 @@
+resource "aws_iam_role" "service_execution_role" {
+  name = format("%s-%s-service-role", var.cluster_name, var.service_name)
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs.amazonaws.com"
+        }
+      }
+    ]
+  })
+  
+}
+
+resource "aws_iam_role_policy" "service_execution_role" {
+  name = format("%s-%s-service-role", var.cluster_name, var.service_name)
+  role = aws_iam_role.service_execution_role.id
+
+    policy = jsondecode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Effect = "Allow"
+                Action = [
+                    "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+                    "elasticloadbalancing:DeregisterTargets",
+                    "elasticloadbalancing:Describe*",
+                    "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+                    "elasticloadbalancing:RegisterTargets",
+                    "ec2:Describe*",
+                    "ec2:AuthorizeSecurityGroupIngress",
+                ]
+                Resource = "*"
+            }
+        ]
+    })
+
+}
